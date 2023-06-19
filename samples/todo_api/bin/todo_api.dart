9f99560f17436d92d74703d278f64034124d29b3
exaby73 <nabeelparkar99@gmail.com>
2023-06-20 05:06:50 +0530

    chore: move package into packages directory

---
+++ samples/todo_api/bin/todo_api.dart
@@ -0,0 +1,5 @@
+import 'package:todo_api/todo_api.dart' as todo_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${todo_api.calculate()}!');
+}
---
+++ samples/todo_api/test/todo_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:todo_api/todo_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
