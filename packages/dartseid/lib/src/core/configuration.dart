b8e48f7728e284348c55f3adc969cdb3cd9f287a
exaby73 <nabeelparkar99@gmail.com>
2023-06-29 07:13:49 +0530

    add: dartseid_views package

--- packages/dartseid/lib/src/core/configuration.dart
+++ packages/dartseid/lib/src/core/configuration.dart
@@ -7,8 +7,11 @@ class DartseidConfiguration {
   /// The directory where the static files are located.
   static Directory staticFilesDirectory = Directory('static');
 
-  /// The directory where the views are located. To be used with dartseid_views.
+  /// The directory where the views are located. To be used with [`dartseid_views`](https://pub.dev/packages/dartseid_views).
   static Directory viewsDirectory = Directory('views');
+
+  /// The extension of the views files. To be used with [`dartseid_views`](https://pub.dev/packages/dartseid_views).
+  static String viewsExtension = '.html';
 
   DartseidConfiguration._();
 }
