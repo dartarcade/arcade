b87ab6e9f6e97e43348404ea21c3511979a1ff1f
exaby73 <nabeelparkar99@gmail.com>
2023-07-08 09:31:25 +0530

    feat(logger): Initial commit

---
+++ packages/dartseid_logger/bin/dartseid_logger.dart
@@ -0,0 +1,5 @@
+import 'package:dartseid_logger/dartseid_logger.dart' as dartseid_logger;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartseid_logger.calculate()}!');
+}
---
+++ packages/dartseid_logger/test/dartseid_logger_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartseid_logger/dartseid_logger.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
