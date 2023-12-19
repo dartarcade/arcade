d181c0f06b597078bb7a0955f346949389cc76bd
exaby73 <nabeelparkar99@gmail.com>
2023-12-19 09:50:50 +0530

    feat: add support for global before, after and afterWebSocket hooks

--- packages/arcade/example/lib/core/routes.dart
+++ packages/arcade/example/lib/core/routes.dart
@@ -8,6 +8,21 @@ import 'package:arcade_example/core/middlewares.dart';
 final exampleController = ExampleController();
 
 void defineRoutes() {
+  Route.registerGlobalBeforeHook((context) {
+    print('Global before hook');
+    return context;
+  });
+  
+  Route.registerGlobalAfterHook((context, handleResult) {
+    print('Global after hook');
+    return (context, handleResult);
+  });
+  
+  Route.registerGlobalAfterWebSocketHook((context, handleResult, id) {
+    print('Global after websocket hook for $id');
+    return (context, handleResult, id);
+  });
+  
   Route.get('/').handle(exampleController.index);
 
   Route.get('/get').before(checkAuthMiddleware).handle(exampleController.get);
