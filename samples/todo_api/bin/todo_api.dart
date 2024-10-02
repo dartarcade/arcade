aadfee57a699d6b34be94c7a95edf80c61b0ec39
exaby73 <nabeelparkar99@gmail.com>
2024-10-02 19:00:49 +0530

    sample: Take port from environment at runtime

--- samples/todo_api/bin/todo_api.dart
+++ samples/todo_api/bin/todo_api.dart
@@ -1,7 +1,19 @@
+import 'dart:io';
+
 import 'package:arcade/arcade.dart';
 import 'package:todo_api/core/env.dart';
 import 'package:todo_api/core/init.dart';
 
 Future<void> main() async {
-  return runServer(port: Env.port, init: init);
+  final portFromEnvironment = Platform.environment['PORT'];
+  var port = Env.port;
+  if (portFromEnvironment != null) {
+    port = int.parse(portFromEnvironment);
+  }
+
+  if (port == null) {
+    throw StateError('port is not defined');
+  }
+
+  return runServer(port: port, init: init);
 }
--- samples/todo_api/lib/core/env.dart
+++ samples/todo_api/lib/core/env.dart
@@ -4,8 +4,8 @@ part 'env.g.dart';
 
 @Envied()
 class Env {
-  @EnviedField(varName: 'PORT')
-  static const int port = _Env.port;
+  @EnviedField(optional: true)
+  static const int? port = _Env.port;
 
   @EnviedField()
   static const String databaseUrl = _Env.databaseUrl;
