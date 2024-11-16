82961f27bda68d389c6c5588c94fdabe5d7d64a2
Nabeel Parkar <nabeelparkar99@gmail.com>
2024-11-16 14:14:26 +0530

    feat!: Move views to jinja templates (#38)

--- packages/arcade_views/lib/src/view.dart
+++ packages/arcade_views/lib/src/view.dart
@@ -1,39 +1,15 @@
-import 'dart:io';
-
 import 'package:arcade_config/arcade_config.dart';
-import 'package:mustache_template/mustache_template.dart';
-import 'package:path/path.dart';
+import 'package:jinja/jinja.dart';
+import 'package:jinja/loaders.dart';
 
 String view(String name, [Map<String, dynamic>? data]) {
-  final template = _partialResolver(name);
-  return template.renderString(data ?? {});
-}
-
-Template _partialResolver(String name, [Directory? currentDirectory]) {
-  final viewsDirectory = name.startsWith('/')
-      ? ArcadeConfiguration.viewsDirectory
-      : currentDirectory ?? ArcadeConfiguration.viewsDirectory;
-  final viewsDirectoryExists = viewsDirectory.existsSync();
-  if (!viewsDirectoryExists) {
-    throw Exception('Views directory does not exist.');
-  }
-  final extension = ArcadeConfiguration.viewsExtension;
-  final pathSegments = name.split('/')
-    ..removeWhere((segment) => segment.isEmpty);
-  final fileName = pathSegments.last;
-  pathSegments.removeLast();
-
-  final file = File(
-    joinAll([viewsDirectory.path, ...pathSegments, '$fileName$extension']),
+  const isProd = bool.fromEnvironment('dart.vm.product');
+  final environment = Environment(
+    // ignore: avoid_redundant_argument_values
+    autoReload: !isProd,
+    loader: FileSystemLoader(paths: [ArcadeConfiguration.viewsDirectory.path]),
   );
-  final fileExists = file.existsSync();
-  if (!fileExists) {
-    throw Exception('View file does not exist.');
-  }
-  final content = file.readAsStringSync();
-  return Template(
-    content,
-    lenient: true,
-    partialResolver: (name) => _partialResolver(name, file.parent),
-  );
+  final templateName = '$name${ArcadeConfiguration.viewsExtension}';
+  final template = environment.getTemplate(templateName);
+  return template.render(data);
 }
