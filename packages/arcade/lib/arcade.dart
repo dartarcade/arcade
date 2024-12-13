7ac4e2c5f7c04c9b3b7ad8a571385fbc273a08d0
exaby73 <nabeelparkar99@gmail.com>
2024-12-14 04:11:40 +0530

    feat(arcade): Add RouteMetadata

--- packages/arcade/lib/arcade.dart
+++ packages/arcade/lib/arcade.dart
@@ -3,6 +3,7 @@ export 'package:arcade_logger/arcade_logger.dart' show LogLevel;
 export 'src/core/error_handler.dart';
 export 'src/core/exceptions.dart';
 export 'src/core/hooks.dart';
+export 'src/core/metadata.dart';
 export 'src/http/body_parse_result.dart';
 export 'src/http/request_context.dart';
 export 'src/http/route.dart' hide routes;
