4ef843cedc8eeb1963f5cd0e2ea4fe752353f808
exaby73 <nabeelparkar99@gmail.com>
2024-12-25 07:22:47 +0530

    feat(aracade_views): Add support for pointing to views inside packages

--- packages/arcade_views/lib/src/view.dart
+++ packages/arcade_views/lib/src/view.dart
@@ -1,13 +1,52 @@
+import 'dart:io';
+import 'dart:isolate';
+
 import 'package:arcade_config/arcade_config.dart';
+import 'package:arcade_logger/arcade_logger.dart';
 import 'package:jinja/jinja.dart';
 import 'package:jinja/loaders.dart';
+import 'package:path/path.dart';
 
-String view(String name, [Map<String, dynamic>? data]) {
+typedef PackageViews = ({String packagePath, String viewsPath});
+
+const logger = Logger('arcade_views');
+
+String view(
+  String name, [
+  Map<String, dynamic>? data,
+  PackageViews? packageViews,
+]) {
   const isProd = bool.fromEnvironment('dart.vm.product');
+  Directory? packageViewsDirectory;
+  if (packageViews != null) {
+    final uri = Isolate.resolvePackageUriSync(
+      Uri.parse(packageViews.packagePath),
+    );
+    if (uri == null) {
+      throw StateError('Package path not found: ${packageViews.packagePath}');
+    }
+    // By default, this will point to lib. We need to go up one level to get the root.
+    final dir = Directory.fromUri(uri).parent;
+    packageViewsDirectory = Directory(join(dir.path, packageViews.viewsPath));
+
+    if (!packageViewsDirectory.existsSync()) {
+      throw StateError(
+        'Package views directory not found: ${packageViewsDirectory.path}',
+      );
+    }
+  }
+
+  final paths = [
+    if (packageViewsDirectory != null) packageViewsDirectory.path,
+    ArcadeConfiguration.viewsDirectory.path,
+  ];
+  logger.debug('Loading views from: $paths');
   final environment = Environment(
     // ignore: avoid_redundant_argument_values
     autoReload: !isProd,
-    loader: FileSystemLoader(paths: [ArcadeConfiguration.viewsDirectory.path]),
+    loader: FileSystemLoader(
+      paths: paths,
+    ),
   );
   final templateName = '$name${ArcadeConfiguration.viewsExtension}';
   final template = environment.getTemplate(templateName);
